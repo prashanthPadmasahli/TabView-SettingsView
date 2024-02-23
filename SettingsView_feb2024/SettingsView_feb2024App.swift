1cfb45c17d7b9b7dd5a64baf998004290ba039fe
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-02-23 16:03:58 +0530

    Initial Commit

---
+++ SettingsView_feb2024/SettingsView_feb2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsView_feb2024App.swift
+//  SettingsView_feb2024
+//
+//  Created by mac on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SettingsView_feb2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
